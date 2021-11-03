338c534a173bd6aca31ac331b7a6180c65106adc
Varma <rahulVarmabh@gmail.com>
2021-11-03 16:48:32 +0530

    Add project files.

---
+++ Proj/Models/salary.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Proj.Models
+{
+   
+    public class Salary
+    {
+        public double basicpay { get; set; }
+        public double da { get; set; }
+        public double hra { get; set; }
+        public double pf { get; set; }
+        public double tax { get; set; }
+        public double netsalary { get; set; }
+
+    }
+    public class SalaryModel
+    {
+        public List<Salary> SalaryList { get; set; }
+    }
+}
